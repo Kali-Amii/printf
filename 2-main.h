4e5516a47078568909d22965bf84a89e0ce560b7
Pa-vic <victorndudim2000@gmail.com>
2023-07-26 03:10:03 -0700

    All files commit

--- 2-main.h
+++
@@ -1,6 +0,0 @@
-#ifndef _2-MAIN_H_
-#define _2-MAIN_H_
-
-int print_unsigned_base2(va_list args);
-
-#endif /* _2-MAIN_H_ */
--- umain.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UMAIN_H_
-#define _UMAIN_H_
-
-#include <stdarg.h>
-
-int print_unsigned_base2(va_list args);
-
-#endif /* _UMAIN_H_ */
